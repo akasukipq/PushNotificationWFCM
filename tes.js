0945209d416938b985bff4351d30069816145953
akasukipq <phatnguyen240899@gmail.com>
2019-11-03 22:09:09 +0700

    add seminar proj ver1.0

---
+++ tes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class tes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> tes </Text>
+      </View>
+    );
+  }
+}
